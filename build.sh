2f3973f3aaa69437e639c51d0199896e76b45c71
Tomas Carnecky <tom@dbservice.com>
2006-10-20 17:40:42 +0000

    get rid of the SConstruct

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-LIBDIR=${1:-'lib'}
+LIBDIR=${LIBDIR:-'lib'}
+CC=${CC:-'gcc'}
 
 CopyLib () {
 	if [ ! -e "/usr/$LIBDIR/$1" ]; then
@@ -24,14 +25,14 @@ echo "Cleaning directory..."
 rm -Rf $HOME/.yukon/$LIBDIR && mkdir -p $HOME/.yukon/$LIBDIR
 
 echo "Compiling patcher..."
-scons -s patcher
+$CC -o patcher -std=c99 -pipe -O3 src/patcher.c
 
 echo "Duplicating libraries..."
 CopyLib libGL.so libFG.so
 CopyLib libX11.so libX13.so
 
 echo "Building yukon..."
-LIBPATH=$HOME/.yukon/$LIBDIR scons -s libyukon.so
+$CC -shared -o libyukon.so -Iinclude -fPIC -L$HOME/.yukon/$LIBDIR -lX13 -lFG src/yukon.c
 
 echo "Restoring libraries..."
 cp libyukon.so $HOME/.yukon/$LIBDIR/
@@ -40,7 +41,8 @@ RestoreLib libGL.so
 RestoreLib libX11.so
 
 echo "Cleaning up..."
-scons -s -c
+rm patcher
+rm libyukon.so
 
 echo ""
 echo "Please make sure LD_LIBRARY_PATH points to \"\$HOME/.yukon/$LIBDIR\""

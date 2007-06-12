5a1f070af67bfb728350a96df684d374524b252c
Tomas Carnecky <tom@dbservice.com>
2007-06-12 19:24:19 +0000

    Change spelling to make it more clear how to fix yukon.

--- tools/post-install-check.sh
+++ tools/post-install-check.sh
@@ -3,24 +3,36 @@
 # We need an OpenGL application for the tests.
 GLXGEARS=`which glxgears 2>/dev/null`
 if [ "$?" = "1" ]; then
-	echo -e "glxgears not found.\n - Install a package that provides the glxgears application." && exit 1
+	echo "glxgears not found."
+	echo " - Install a package that provides the glxgears application."
+
+	exit 1
 fi
 
 # Check whether the yukon script is installed
 if ! which yukon 1>/dev/null 2>&1; then
-	echo -e "yukon script not found.\n - Add \$PREFIX/bin to \$PATH." && exit 1
+	echo "yukon script not found."
+	echo " - Add \$PREFIX/bin to \$PATH."
+
+	exit 1
 fi
 
 # Does the yukon script set the correct LD_LIBRARY_PATH ?
 RET=`yukon printenv | grep LD_LIBRARY_PATH | grep yukon:`
 if [ -z "${RET}" ]; then
-	echo -e "The yukon script doesn't work correctly.\n - Copy sysconf to /etc/yukon/system or \$HOME/.yukon/system" && exit 1
+	echo "The yukon script doesn't work correctly."
+	echo " - Copy sysconf to /etc/yukon/system or \$HOME/.yukon/system"
+
+	exit 1
 fi
 
 # Does the application pick up the modified libraries ?
 RET=`yukon ldd ${GLXGEARS} | grep yukon/libGL.so.native`
 if [ -z "${RET}" ]; then
-	echo -e "glxgears doesn't use the modified libraries.\n - This won't happen unless you edit the sysconf file by hand or delete the modified libraries" && exit 1
+	echo "glxgears doesn't appear to be using the modified libraries."
+	echo " - Make sure yukon is installed (run 'make install' as root)"
+
+	exit 1
 fi
 
 # Extract the path where the yukon libraries are installed in
@@ -29,7 +41,10 @@ YPATH=`echo ${RET} | sed -e 's#.*\s/\(.*\)/libGL.so.native.*#/\1#'`
 # Check whether yukon-core-lib finds libseom.so.?
 RET=`yukon ldd "${YPATH}/yukon-core-lib" | grep -E libseom.so.? | grep "not found"`
 if [ -n "${RET}" ]; then
-	echo -e "libseom.so.? not found.\n - Make sure this library is installed and the linker can find it" && exit 1
+	echo "libseom.so.? not found."
+	echo " - Make sure this library is installed and the linker can find it"
+
+	exit 1
 fi
 
 echo "Yukon seems installed correctly!"
